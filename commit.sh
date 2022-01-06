1130ff9f0830b7ade3570f29aaf241a8ef1ab75e
baimamboukar <baimamboukar@gmail.com>
2022-01-06 01:56:45 +0100

    adding commit.sh

--- commit.sh
+++ commit.sh
@@ -1,6 +1,24 @@
 for file in * 
 do
         git add $file
-        git commit -m "commit $file"
+        git commit -m "adding $file"
+        git push
+done
+
+cd datasource
+for file in * 
+do
+        git add $file
+        git commit -m "adding $file"
+        git push
+done
+
+cd - 
+
+cd cleandata
+for file in * 
+do
+        git add $file
+        git commit -m "adding $file"
         git push
 done
